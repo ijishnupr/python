b71c3f16a0f2d072468b7809fca0af7177620b1b
jishnu pr <ijishnupr@gmail.com>
2023-06-12 19:45:26 +0530

    lab 2 and 3 commit

---
+++ lab2.py
@@ -0,0 +1,6 @@
+num=int(input("enter a number"))
+if num%2==0:
+    print("hello you entered a even no:")
+else:
+    print("you you entered a odd no:")
+    
---
+++ lab4.py
@@ -0,0 +1,4 @@
+num=int(input("enter the no:"))
+for i in range(num):
+    if num/i==0:
+        print(i)
